b23e5a71b357739bf1636d1c4600d6570a8be438
raham24 <rahamriaz@gmail.com>
2025-11-24 13:58:41 -0500

    Added CORS headers

--- src/api/test.cpp
+++ src/api/test.cpp
@@ -4,13 +4,28 @@
 
 void handleTestRoute() {
     Serial.begin(9600);
+
+    // Add CORS headers
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.sendHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    server.sendHeader("Access-Control-Allow-Headers", "Content-Type");
+
     String message = "Hello from ESP32 API! Test route is working.";
     server.send(200, "text/plain", message);
     Serial.println("Test route accessed");
 }
 
+void handleTestOptions() {
+    // Handle preflight OPTIONS request
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.sendHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    server.sendHeader("Access-Control-Allow-Headers", "Content-Type");
+    server.send(204);
+}
+
 void registerTestRoutes() {
     Serial.begin(9600);
     server.on("/test", HTTP_GET, handleTestRoute);
+    server.on("/test", HTTP_OPTIONS, handleTestOptions);
     Serial.println("Test routes registered: /test");
 }
